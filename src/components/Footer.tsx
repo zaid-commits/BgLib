9f41144577f84dde1df23269f669404d2421be24
Zaid Rakhange <zaidcommits.github@gmail.com>
2024-10-12 22:27:40 +0530

    updated href

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,9 @@
 export const Footer = () => (
-    <footer className="bg-gray-100 py-8 px-4 text-center">
-        <div className="mt-4">
-            <a href="https://zed.impic.tech" className="text-gray-600 hover:text-black">Under development by Zaid at <a href="https://community.impic.tech">Impic</a></a>
+    <footer className="bg-white text-center">
+        <div className="mt-4 my-2">
+            <a href="https://zed.impic.tech">
+                Under development by <span className="text-blue-500 hover:text-blue-500">Zaid</span> at <a href="https://community.impic.tech" className="text-blue-500 hover:text-blue-500">Impic</a>
+            </a>
         </div>
     </footer>
-)
+);
