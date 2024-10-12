00631b317c9047edfc99d1acb16ef84b7e8d60f0
Zaid Rakhange <zaidcommits.github@gmail.com>
2024-10-12 23:01:00 +0530

    Update developer name in Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ export const Footer = () => (
     <footer className="bg-white text-center">
         <div className="mt-4 my-2">
             <a href="https://zed.impic.tech">
-                Under development by <span className="text-blue-500 hover:text-blue-500">Zaid</span> at <a href="https://community.impic.tech" className="text-blue-500 hover:text-blue-500">Impic</a>
+                Under development by <span className="text-blue-500 hover:text-blue-500">Zed</span> at <a href="https://community.impic.tech" className="text-blue-500 hover:text-blue-500">Impic</a>
             </a>
         </div>
     </footer>
