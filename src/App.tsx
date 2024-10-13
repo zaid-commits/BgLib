7bf7b2b9940b96f9a604e97b3b34936dba969287
Zaid Rakhange <zaidcommits.github@gmail.com>
2024-10-13 22:38:37 +0530

    added parallax effect component

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import AboutPage from './pages/AboutPage';  // Import the new AboutPage
 import ButtonsPage from '@/components/Library/ButtonsPage';
 import FormsPage from '@/components/Library/FormsPage';
 import CardsPage from '@/components/Library/CardsPage';
+import ParallaxScrollPage from '@/components/Library/ParallaxScrollPage';
 import 'prismjs/themes/prism-tomorrow.css';
 import 'prismjs/plugins/line-numbers/prism-line-numbers.css';
 
@@ -20,6 +21,7 @@ function App() {
             <Route index element={<ButtonsPage />} />
             <Route path="buttons" element={<ButtonsPage />} />
             <Route path="forms" element={<FormsPage />} />
+            <Route path="parallax-scroll" element={<ParallaxScrollPage />} />
             <Route path="cards" element={<CardsPage />} />
           </Route>
           <Route path="/about" element={<AboutPage />} />  {/* Add this line */}
