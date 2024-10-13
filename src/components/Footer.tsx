539b083e36f33328510e719c3af26401fad1fbaf
Zaid Rakhange <zaidcommits.github@gmail.com>
2024-10-13 08:44:32 +0530

    updated footer href

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => (
     <footer className="pb-8 text-center">
         <p className="text-gray-600">
-            Crafted with ❤️ by <a href="https://zed.impic.tech" className="text-blue-500 hover:text-blue-600 transition-colors">Zed</a> at <a href="https://community.impic.tech" className="text-blue-500 hover:text-blue-600 transition-colors">Impic</a>
+            Crafted with ❤️ by <a href="https://zed.impic.tech" className="text-blue-500 hover:text-blue-600 transition-colors underline">Zed</a> at <a href="https://community.impic.tech" className="text-blue-500 hover:text-blue-600 transition-colors">Impic</a>
         </p>
     </footer>
 );
