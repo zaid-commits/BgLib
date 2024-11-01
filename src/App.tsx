49e7ae3fc1b0e86778f23c214609df2ffa696412
Zaid Rakhange <zaidcommits.github@gmail.com>
2024-11-01 21:48:25 +0530

    fixed the social logos

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import BackgroundLibrary from './pages/BackgroundLibrary';
-import ComponentLibrary from './pages/ComponentLibrary';
 import AboutPage from './pages/AboutPage';
-import ButtonsPage from '@/components/Library/ButtonsPage';
-import FormsPage from '@/components/Library/FormsPage';
-import CardsPage from '@/components/Library/CardsPage';
-import ParallaxScrollPage from '@/components/Library/ParallaxScrollPage';
 import NotFoundPage from './components/Library/404';
-import Navbar from './components/Navbar';
+import { FiGithub, FiTwitter } from 'react-icons/fi'; 
 import 'prismjs/themes/prism-tomorrow.css';
 import 'prismjs/plugins/line-numbers/prism-line-numbers.css';
 
@@ -15,19 +10,28 @@ function App() {
   return (
     <Router>
       <div className="flex flex-col min-h-screen">
-        <Navbar />
-        <div className="flex-grow overflow-y-auto">
+        <div className="flex-grow overflow-y-auto relative">
+          <div className="flex justify-end p-4 absolute top-4 right-4 z-10">
+            <a
+              href="https://github.com/zaid-commits/bglib"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="p-2 text-gray-600 hover:text-indigo-600 transition-colors duration-200 mr-4"
+            >
+              <FiGithub className="w-5 h-5" />
+            </a>
+            <a
+              href="https://twitter.com/zaidcommits"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="p-2 text-gray-600 hover:text-indigo-600 transition-colors duration-200"
+            >
+              <FiTwitter className="w-5 h-5" />
+            </a>
+          </div>
           <Routes>
             <Route path="/" element={<BackgroundLibrary />} />
             <Route path="/backgrounds" element={<BackgroundLibrary />} />
-            <Route path="/components" element={<ComponentLibrary />}>
-              <Route index element={<ButtonsPage />} />
-              <Route path="buttons" element={<ButtonsPage />} />
-              <Route path="forms" element={<FormsPage />} />
-              <Route path="parallax-scroll" element={<ParallaxScrollPage />} />
-              <Route path="cards" element={<CardsPage />} />
-              <Route path="not-found" element={<NotFoundPage />} />
-            </Route>
             <Route path="/about" element={<AboutPage />} />
             <Route path="*" element={<NotFoundPage />} />
           </Routes>
