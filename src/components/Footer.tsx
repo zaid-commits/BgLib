dac3a1613e5461595ea53d68e28e8f8f85f0b516
Zaid Rakhange <zaidcommits.github@gmail.com>
2024-11-03 13:16:47 +0530

    removed padding

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 export const Footer = () => (
-    <footer className="pb-8 text-center">
+    <footer className="text-center m-0 p-0 ">
         <p className="text-gray-600">
             Crafted with ❤️ by <a href="https://zed.impic.tech" className="text-blue-500 hover:text-blue-600 transition-colors underline">Zed</a> at <a href="https://community.impic.tech" className="text-blue-500 hover:text-blue-600 transition-colors">Impic</a>
         </p>
