38eae537d7d63c6865ab17edb1f278ff3922c6f9
Zaid Rakhange <zaidcommits.github@gmail.com>
2024-11-03 13:17:19 +0530

    removed unused route

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import BackgroundLibrary from './pages/BackgroundLibrary';
 import AboutPage from './pages/AboutPage';
 import NotFoundPage from './components/Library/404';
-import { FiGithub, FiTwitter } from 'react-icons/fi'; 
+import { FiGithub, FiTwitter } from 'react-icons/fi';
 import 'prismjs/themes/prism-tomorrow.css';
 import 'prismjs/plugins/line-numbers/prism-line-numbers.css';
 
@@ -31,7 +31,6 @@ function App() {
           </div>
           <Routes>
             <Route path="/" element={<BackgroundLibrary />} />
-            <Route path="/backgrounds" element={<BackgroundLibrary />} />
             <Route path="/about" element={<AboutPage />} />
             <Route path="*" element={<NotFoundPage />} />
           </Routes>
