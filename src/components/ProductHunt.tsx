81d995da49b6abff2e3b667e31d71594bd712860
Zaid Rakhange <zaidcommits.github@gmail.com>
2024-11-03 21:28:21 +0530

    created producthunt badge component

---
+++ src/components/ProductHunt.tsx
@@ -0,0 +1,8 @@
+
+const ProductHuntBadge = () => (
+  <a href="https://www.producthunt.com/posts/bglib?embed=true&utm_source=badge-featured&utm_medium=badge&utm_souce=badge-bglib" target="_blank">
+    <img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=571509&theme=dark" alt="BgLib - Background library for your next product | Product Hunt" style={{ width: '250px', height: '54px' }} />
+  </a>
+)
+
+export default ProductHuntBadge
